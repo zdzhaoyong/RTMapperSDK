4f1dbbe23eb835f734cb628013b0028b5053948e
Shuhui Bu <bushuhui@foxmail.com>
2018-05-28 10:15:50 +0800

    change to RTMapper::create()

--- example-fitgps/example-fitgps.cpp
+++ example-fitgps/example-fitgps.cpp
@@ -390,7 +390,7 @@ public:
             return ;
         }
 
-        RTMapperPtr rtmapper=createRTMapperInstance();
+        RTMapperPtr rtmapper=RTMapper::create();
         if(!rtmapper) {
             LOG(ERROR)<<"Failed to load RTMapperSDK.";
             return;
